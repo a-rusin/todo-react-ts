fd38ac076e7c86cb69f932690f76c69fbf233039
Rusin Alexandr <i@arusin.ru>
2025-04-29 06:59:21 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = "static-app-v1";
-const dynamicCacheName = "dynamic-app-v1";
+const staticCacheName = "static-app-v6";
+const dynamicCacheName = "dynamic-app-v6";
 
 const ASSETS = [];
 
@@ -9,6 +9,12 @@ self.addEventListener("install", async () => {
 
 self.addEventListener("activate", async () => {
   console.log("activate!");
+  const cachesKeysArr = await caches.keys();
+  await Promise.all(
+    cachesKeysArr
+      .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+      .map((key) => caches.delete(key))
+  );
 });
 
 self.addEventListener("fetch", (event) => {
