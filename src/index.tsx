6ecab3f0814526a11eb6a7bc1582e544378aa0c8
Rusin Alexandr <i@arusin.ru>
2025-04-29 06:33:31 +0300

    bugix: исправил баг с путями на gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,23 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { NavigateContextProvider } from "./hooks/useAppNavigate";
 import { AuthProvider } from "./context/AuthContext";
 import * as serviceWorkerRegistration from "./services/serviceWorker.sevice";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <NavigateContextProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
     </NavigateContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 serviceWorkerRegistration.LocalServiceWorkerRegister();
